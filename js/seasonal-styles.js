4c8504854260349349ce9e967a45128194087566
sadiestevenson <sadestevenson4@gmail.com>
2022-07-31 15:34:19 -0700

    seasonal styles

--- js/seasonal-styles.js
+++ js/seasonal-styles.js
@@ -5,3 +5,53 @@ $("document").ready(function(){
         alert(season);
     });
 });
+
+switch(season){
+
+case "Spring":{
+    $("html").css("background-color", "pink");
+    $("#wear").attr("src","images/###");
+    $("#logo").attr("src","images/###");
+    $("header h3").text("Spring is here!");
+
+};
+break;
+
+case "Summer":{
+    $("html").css("background-color", "yellow");
+    $("#wear").attr("src","images/###");
+    $("#logo").attr("src","images/###");
+    $("header h3").text("Spring is here!");
+
+};
+break;
+
+case "Fall":{
+    $("html").css("background-color", "brown");
+    $("#wear").attr("src","images/###");
+    $("#logo").attr("src","images/###");
+    $("header h3").text("Spring is here!");
+
+};
+break;
+
+case "Winter":{
+    $("html").css("background-color", "blue");
+    $("#wear").attr("src","images/###");
+    $("#logo").attr("src","images/###");
+    $("header h3").text("Spring is here!");
+
+};
+break;
+
+case "Default":{
+    $("html").css("background-color", "grey");
+    $("#wear").attr("src","images/###");
+    $("#logo").attr("src","images/###");
+    $("header h3").text("Spring is here!");
+
+};
+break;
+
+
+};
